f508cbcdc500d1cafdb87677ea4f7ee2ab803123
Michael Wu <cheeserules43@gmail.com>
2019-02-14 02:34:47 -0800

    added submission zip

--- hw3/zip_hw3.sh
+++ hw3/zip_hw3.sh
@@ -1,2 +1,2 @@
-rm -f HW3.zip 
-zip -r HW3.zip . -x "*.git*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*requirements.txt" ".env/*" "zip_hw3.sh" "*.pyc" "HW3_programming_instructions.pdf"
+rm -f HW3.zip
+zip -r HW3.zip . -x "*.git*" "*cs145/datasets*" "*.ipynb_checkpoints*" "*README.md" "*requirements.txt" ".env/*" "zip_hw3.sh" "*.pyc" "HW3_programming_instructions.pdf"
