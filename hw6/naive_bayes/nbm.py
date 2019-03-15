5f7e7075c04e57c8a7fe6ac55ecd56f41782a73a
Michael Wu <cheeserules43@gmail.com>
2019-03-15 01:30:24 -0700

    finished first half of hw6

--- hw6/naive_bayes/nbm.py
+++ hw6/naive_bayes/nbm.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,35 +9,40 @@ rcParams['figure.figsize'] = 10, 10
 from sklearn.metrics import confusion_matrix
 
 class NB_model():
-    def __init__(self): 
-        self.pi = {} # to store prior probability of each class 
+    def __init__(self):
+        self.pi = {} # to store prior probability of each class
         self.Pr_dict = None
         self.num_vocab = None
         self.num_classes = None
-    
+
     def fit(self, train_data, train_label, vocab, if_use_smooth=True):
         # get prior probabilities
         self.num_vocab = len(vocab['index'].tolist())
         self.get_prior_prob(train_label)
-        # ================== YOUR CODE HERE ==========================
-        # Calculate probability of each word based on class 
-        # Hint: Store each probability value in matrix or dict: self.Pr_dict[classID][wordID] or Pr_dict[wordID][classID])
-        # Remember that there are possible NaN or 0 in Pr_dict matrix/dict. Use smooth method
-        
-        # ============================================================
+        self.Pr_dict = df_train.groupby(['classIdx', 'wordIdx']).sum().reset_index().pivot('wordIdx', 'classIdx', 'count').fillna(0).to_dict()
+        for i in range(1, self.num_classes+1):
+            sum = 0
+            for j in range(1, self.num_vocab+1):
+                if if_use_smooth:
+                    self.Pr_dict[i][j] = self.Pr_dict[i].setdefault(j, 0) + 1
+                else:
+                    self.Pr_dict[i].setdefault(j, 0)
+                sum += self.Pr_dict[i][j]
+            for j in range(1, self.num_vocab+1):
+                self.Pr_dict[i][j] /= float(sum)
         print("Training completed!")
-    
+
     def predict(self, test_data):
         test_dict = test_data.to_dict() # change dataframe to dict
         new_dict = {}
         prediction = []
-        
+
         for idx in range(len(test_dict['docIdx'])):
             docIdx = test_dict['docIdx'][idx]
             wordIdx = test_dict['wordIdx'][idx]
             count = test_dict['count'][idx]
-            try: 
-                new_dict[docIdx][wordIdx] = count 
+            try:
+                new_dict[docIdx][wordIdx] = count
             except:
                 new_dict[test_dict['docIdx'][idx]] = {}
                 new_dict[docIdx][wordIdx] = count
@@ -45,28 +51,20 @@ class NB_model():
             score_dict = {}
             #Creating a probability row for each class
             for classIdx in range(1,self.num_classes+1):
-                score_dict[classIdx] = 0
-                # ================== YOUR CODE HERE ==========================
-                ### Implement the score_dict for all classes for each document
-                ### Remember to use log addtion rather than probability multiplication
-                ### Remember to add prior probability, i.e. self.pi
-
-                # ============================================================
+                score_dict[classIdx] = math.log(self.pi[classIdx])
+                for word, count in new_dict[docIdx].items():
+                    score_dict[classIdx] += count * math.log(self.Pr_dict[classIdx][word])
             max_score = max(score_dict, key=score_dict.get)
             prediction.append(max_score)
         return prediction
-                    
-    
+
+
     def get_prior_prob(self,train_label, verbose=True):
         unique_class = list(set(train_label))
         self.num_classes = len(unique_class)
         total = len(train_label)
         for c in unique_class:
-            # ================== YOUR CODE HERE ==========================
-            ### calculate prior probability of each class ####
-            ### Hint: store prior probability of each class in self.pi
-
-            # ============================================================
+            self.pi[c] = train_label.count(c) / float(total)
         if verbose:
             print("Prior Probability of each class:")
             print("\n".join("{}: {}".format(k, v) for k, v in self.pi.items()))
@@ -83,10 +81,10 @@ df_train = pd.read_csv(train_data, delimiter=' ', names=['docIdx', 'wordIdx', 'c
 test_data = open('./dataset/test.data')
 df_test = pd.read_csv(test_data, delimiter=' ', names=['docIdx', 'wordIdx', 'count'])
 # read vocab
-vocab = open('./dataset/vocabulary.txt') 
-vocab_df = pd.read_csv(vocab, names = ['word']) 
-vocab_df = vocab_df.reset_index() 
-vocab_df['index'] = vocab_df['index'].apply(lambda x: x+1) 
+vocab = open('./dataset/vocabulary.txt')
+vocab_df = pd.read_csv(vocab, names = ['word'])
+vocab_df = vocab_df.reset_index()
+vocab_df['index'] = vocab_df['index'].apply(lambda x: x+1)
 
 #Add label column to original df_train (for better implementation)
 docIdx = df_train['docIdx'].values
